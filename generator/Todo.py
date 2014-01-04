1d6781368dc30b769f400b398e703ca54c9b166c
Andre Aboulian <aandre@mit.edu>
2014-01-03 18:09:04 -0800

    Fields Removed, Half/Full Page options, Single Week, No Covers

--- generator/Todo.py
+++ generator/Todo.py
@@ -47,21 +47,25 @@ from Logo import placeLogo
 # #####################################|####################################
 
 def weeklyTodo(
-        filename, weekone, weektwo=None, pagesize=letter, margins=0.5*inch,
+        filename, items, pagesize=letter, margins=0.5*inch, booklet=0,
         binding=0.25*inch, gridline=0.7, gridcolor=20, **excessParams):
     """
     Generates a biweekly todo list booklet.
 
     Keyword arguments:
     filename -- output PDF document name
-    weekone -- list of topics for a given week
-    weektwo -- list of topics for an alternate week, None for single week
+    items -- list of topics for a given week
     pagesize -- size of page as (width, height) tuple
     margins -- size of margins around page
+    booklet -- flag to generate half-page booklet; full-page if false
     binding -- size of spacing between list and binding on each page
     gridline -- thickness of lines around cells
     gridcolor -- color of grid lines around cells
     """
+    # landscape orientation
+    if not booklet:
+        pagesize = pagesize[::-1]
+
     # canvas attributes
     page = canvas.Canvas(filename, pagesize=pagesize)
     page.setTitle("Weekly Todo List by Give Sheet")
@@ -75,43 +79,51 @@ def weeklyTodo(
                    ("OstrichSans-Bold", "support/fonts/ostrich/OstrichSans-Black.ttf"),
                    ])
 
-    # generate pages
-    _makeItemizedTodo(page=page, items=weekone, pagesize=pagesize, margins=margins,
-                     binding=binding, gridline=gridline, gridcolor=gridcolor, coverpage=1)
-    page.showPage() # new page
-    if weektwo != None:
-        _makeItemizedTodo(page=page, items=weektwo, pagesize=pagesize, margins=margins,
-                         binding=binding, gridline=gridline, gridcolor=gridcolor)
+    page_w, page_h = pagesize
 
-    placeLogo(margins, pagesize, canvas, quadrant=1)
-    placeLogo(margins, pagesize, canvas, quadrant=4)
-    
+    # generate pages
+    if booklet:
+        _makeItemizedTodo(page=page, items=items, origin=(0,0), targetsize=(page_w,page_h/2),
+                          margins=margins, binding=binding, gridline=gridline, gridcolor=gridcolor)
+        _makeItemizedTodo(page=page, items=items, origin=(0,page_h/2), targetsize=(page_w,page_h/2),
+                          margins=margins, binding=binding, gridline=gridline, gridcolor=gridcolor)
+        placeLogo(margins, pagesize, canvas, quadrant=1)
+        placeLogo(margins, pagesize, canvas, quadrant=4)
+    else:
+        _makeItemizedTodo(page=page, items=items, origin=(0,0), targetsize=(page_w,page_h),
+                          margins=margins, binding=binding, gridline=gridline, gridcolor=gridcolor)
+        placeLogo(margins, (page_h, ), canvas, quadrant=4)
+
     page.save()
 
-def _makeItemizedTodo(page, items, pagesize, margins, binding,
-                     gridline, gridcolor, coverpage=0, **excessParams):
+def _makeItemizedTodo(page, items, origin, targetsize, margins, binding,
+                     gridline, gridcolor, **excessParams):
     """
     Creates a single week of an itemized todo list.
 
     Keyword arguments:
     page -- a Canvas instance on which to draw
     items -- list of topics for a given week
-    pagesize -- size of page as (width, height) tuple
+    origin -- origin of drawing area as (x, y) tuple
+    targetsize -- size of target drawing area as (width, height) tuple
     margins -- size of margins around page
     binding -- size of spacing between list and binding on each page
     gridline -- thickness of lines around cells
     gridcolor -- color of grid lines around cells
-    coverpage -- flag for including cover pages of booklet
     """
+    page.saveState()
+
+    page.translate(*origin)
+
     # dimensions and layout
     key_size = 20
     days_label_size = 12
     topic_label_size = 20
 
-    page_w, page_h = pagesize
+    page_w, page_h = targetsize
 
     area_w, area_h = (page_w - 2*margins - 2*binding) / 2, \
-                     (page_h - 4*margins) / 2
+                     (page_h - 2*margins)
     grid_w, grid_h = area_w - topic_label_size, \
                      area_h - key_size - days_label_size
     cell_w, cell_h = grid_w / 3, grid_h / (len(items) + 1)
@@ -171,85 +183,19 @@ def _makeItemizedTodo(page, items, pagesize, margins, binding,
                         ('VALIGN', (2,0), (2,0), 'MIDDLE'),
                         ]))
 
-    # calculate quadrant locations
-    quad_locs = list() # indicies 0-3 correspond to quadrants I-IV, resp.
-    quad_locs.append((margins + area_w + 2*binding, margins + area_h + 2*margins, right_table, False)) # Q1
-    quad_locs.append((margins, margins + area_h + 2*margins, left_table, True)) # QII
-    quad_locs.append((margins, margins, left_table, True)) # QIII
-    quad_locs.append((margins + area_w + 2*binding, margins, right_table, False)) # QIV
-
-    def _drawChart(loc_x, loc_y, table, onLeft):
+    def _drawTable(table, location):
+        loc_x, loc_y = location
         frame = Frame(loc_x, loc_y, area_w, area_h, showBoundary=0,
                       leftPadding=0, bottomPadding=0, rightPadding=0, topPadding=0)
         frame.addFromList([table], page)
-        _drawListFields(page, origin=(loc_x+topic_label_size,loc_y), layout=(3,len(items)+1),
-                   size=(cell_w,cell_h))
-        week_label_abs = loc_x + topic_label_size + 0.5*cell_w, \
-                         loc_y + grid_h + days_label_size + 0.5*(key_size - 12)
-        if onLeft:
-            _drawWeekField(page, location=week_label_abs, size=(1.5*cell_w, 12))
 
     # draw individual graphs
-    for loc_x, loc_y, table, onLeft in quad_locs:
-        _drawChart(loc_x, loc_y, table, onLeft)
+    _drawTable(right_table, (margins + area_w + 2*binding, margins))
+    _drawTable(left_table, (margins, margins))
 
-    if coverpage:
-        page.showPage()
-        for loc_x, loc_y, table, onLeft in [quad_locs[0]] + [quad_locs[2]]: # QI AND QIII
-            _drawChart(loc_x, loc_y, table, onLeft)
+    page.restoreState()
 
-def _drawListFields(page, origin, layout, size, padding=2):
-    """
-    Draws fillable text fields in the specified locations.
-
-    Keyword arguments:
-    page -- a Canvas instance on which to draw
-    origin -- origin of field grid as (x,y) tuple
-    layout -- layout of cells as (x,y) tuple
-    size -- size of individual text fields as (w,h) tuple
-    padding -- inner padding for text fields
-    """
-    # dimensions and properties
-    cells_x, cells_y = layout
-    cells_w, cells_h = size
-    field_w, field_h = cells_w - 2*padding, cells_h - 2*padding
-    tid = _nextID()
-    
-    #draw fields onto canvas
-    x = origin[0] + padding
-    for i in xrange(cells_x):
-        y = origin[1] + padding
-        for j in xrange(cells_y):
-            title = 'CELL_(' + str(i + 1) + ',' + str(cells_y - j) + ')_' + str(tid)
-            
-            # Courier-Oblique buggy in Preview (line-end quote) and Acrobat (black outline)
-            page.setLineWidth(50)
-            page.setStrokeColor(colors.green)
-            page._setStrokeAlpha(0)
-            page.setFillColor(colors.black) # font color
-            page.setFont('Times-Italic',8)
-            textFieldAbsolute(page, title, x, y, field_w, field_h, multiline=1)
-            # DEBUG
-            # page.rect(x,y,field_w,field_h)
-            # page.drawString(x,y+14,title)
-            y += cells_h
-        x += cells_w
-
-def _drawWeekField(page, location, size, padding=2):
-    """
-    Draws a single-line fillable text field in the specified location.
-
-    Keyword arguments:
-    page -- a Canvas instance on which to draw
-    location -- location to draw fields as (x,y) tuple
-    size -- size of text field as (w,h) tuple
-    padding -- inner padding for text field
-    """
-    (x, y), (w, h) = location, size
-    title = 'WEEK_' + str(_currentID())
-    page.setFont('Times-Roman',12)
-    textFieldAbsolute(page, title, x, y, w, h, maxlen=50, multiline=0)
-
+# ID number generation
 idnum = 0
 def _nextID():
     global idnum
@@ -283,4 +229,4 @@ def _registerFonts(fontlist):
 
 
 if __name__ == '__main__':
-    weeklyTodo("output.pdf",weekone=['A','B','C','D'],weektwo=['1','2','3'])
+    weeklyTodo("output.pdf",items=['A','B','C','D'],booklet=1)
