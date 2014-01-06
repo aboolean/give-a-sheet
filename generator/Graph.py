9e0cc56fe84cad16f583480d229e549759576553
Andre Aboulian <aandre@mit.edu>
2014-01-06 10:22:13 -0800

    Formatting Review

--- generator/Graph.py
+++ generator/Graph.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import math
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch, mm
@@ -6,11 +9,25 @@ from reportlab.lib import colors
 from reportlab.platypus import Table, TableStyle, Frame
 
 from Logo import placeLogo
+from Coloring import grey
+
 
 def cartesian(
-        filename, pagesize=letter, margins=0.5*inch, spacer=0.25*inch, gridspace=0.25*inch,
-        checkered=0, gridline=0.5, boxline=1, checkeredcolor=10, gridcolor=20, boxcolor=80,
-        bgndcolor=0, layout=(1, 1), **excessParams):
+    filename,
+    pagesize=letter,
+    margins=0.5 * inch,
+    spacer=0.25 * inch,
+    gridspace=0.25 * inch,
+    checkered=0,
+    gridline=0.5,
+    boxline=1,
+    checkeredcolor=10,
+    gridcolor=20,
+    boxcolor=80,
+    bgndcolor=0,
+    layout=(1, 1),
+    **excessParams
+    ):
     """
     Generates a page of Cartesian graph paper.
 
@@ -29,16 +46,19 @@ def cartesian(
     bgndcolor -- color of background of each cell
     layout -- number of graphs per page in (x, y) tuple
     """
+
     # dimensions and layout
-    up_w, up_h = layout if layout[0] > 0 and layout[1] > 0 else (1, 1)
-    page_w, page_h = pagesize
-    area_w, area_h = (page_w - 2 * margins - (up_w - 1) * spacer) / up_w, \
-                     (page_h - 2 * margins - (up_h - 1) * spacer) / up_h
+
+    (up_w, up_h) = (layout if layout > (0, 0) else (1, 1))
+    (page_w, page_h) = pagesize
+    area_w = (page_w - 2 * margins - (up_w - 1) * spacer) / up_w
+    area_h = (page_h - 2 * margins - (up_h - 1) * spacer) / up_h
+
     if area_w < 0 or area_h < 0:
-        raise ValueError("The specified dimensions do no fit on the page.")
-    
+        raise ValueError('Specified dimensions do not fit on page.')
+
     frame_locs = list()
-    
+
     x = margins  # begin after left margin
     for frame_x in xrange(up_w):
         y = margins  # begin above lower margin
@@ -48,23 +68,53 @@ def cartesian(
         x += area_w + spacer
 
     # draw result
+
     page = canvas.Canvas(filename, pagesize=pagesize)
     _makeCartesianGrids(
-            page=page, locations=frame_locs, size=(area_w, area_h), gridspace=gridspace,
-            checkered=checkered, gridline=gridline, boxline=boxline, checkeredcolor=checkeredcolor,
-            gridcolor=gridcolor, boxcolor=boxcolor, bgndcolor=bgndcolor)
+        page=page,
+        locations=frame_locs,
+        size=(area_w, area_h),
+        gridspace=gridspace,
+        checkered=checkered,
+        gridline=gridline,
+        boxline=boxline,
+        checkeredcolor=checkeredcolor,
+        gridcolor=gridcolor,
+        boxcolor=boxcolor,
+        bgndcolor=bgndcolor,
+        )
+
     placeLogo(margins, pagesize, canvas, quadrant=4)
 
-    page.setTitle("Cartesian Graph Paper by Give Sheet")
-    page.setAuthor("Andre Aboulian via Give Sheet")
-    page.setSubject("Page Template")
-    page.setKeywords(['graph','cartesian','givesheet','pdf','grid','template','paper'])
+    page.setTitle('Cartesian Graph Paper by Give Sheet')
+    page.setAuthor('Andre Aboulian via Give Sheet')
+    page.setSubject('Page Template')
+    page.setKeywords([
+        'graph',
+        'cartesian',
+        'givesheet',
+        'pdf',
+        'grid',
+        'template',
+        'paper',
+        ])
     page.save()
 
+
 def _makeCartesianGrids(
-        page, locations, size, gridspace, checkered,
-        gridline, boxline, checkeredcolor, gridcolor, boxcolor, bgndcolor,
-        **excessParams):
+    page,
+    locations,
+    size,
+    gridspace,
+    checkered,
+    gridline,
+    boxline,
+    checkeredcolor,
+    gridcolor,
+    boxcolor,
+    bgndcolor,
+    **excessParams
+    ):
     """
     Places a Cartesian graph in the specified location on the canvas.
 
@@ -81,50 +131,77 @@ def _makeCartesianGrids(
     boxcolor -- color of box surrounding graph(s)
     bgndcolor -- color of background of each cell
     """
-    # dimensions
-    area_w, area_h = size
 
-    # compute grid spacing
-    cells_x = int(area_w / gridspace)
-    cells_y = int(area_h / gridspace)
-    if cells_x < 1 or cells_y < 1:
-        raise ValueError("The specified dimensions do no fit on the page.")
-    grid_w = (cells_x * (gridspace)) + boxline
-    grid_h = (cells_y * (gridspace)) + boxline
+    # dimensions and spacing
+
+    (area_w, area_h) = size
+
+    (cells_x, cells_y) = (int(area_w / gridspace), int(area_h / gridspace))
+
+    grid_w = cells_x * gridspace + boxline
+    grid_h = cells_y * gridspace + boxline
+
     xMargins = (area_w - grid_w) / 2
     yMargins = (area_h - grid_h) / 2
 
+    if cells_x < 1 or cells_y < 1:
+        raise ValueError('Specified dimensions do not fit on page.')
+
     # create plain table
+
     data = [['' for col in xrange(cells_x)] for row in xrange(cells_y)]
     table = Table(data, colWidths=gridspace, rowHeights=gridspace)
-    
-    # checkered grid
+
+    # checkered grid or shaded background
+
     pattern = list()
+
     if checkered:
-        for y in xrange(cells_y):
-            for x in xrange(y % 2, cells_x, 2):
-                pattern.append(('BACKGROUND', (x, y), (x, y), colors.CMYKColor(black=0.01 * checkeredcolor)))
+        checkered_cells = [(x, y) for x in xrange(y % 2, cells_x, 2) for y in
+                           xrange(cells_y)]
+        for cell in checkered_cells:
+            pattern.append(('BACKGROUND', cell, cell, grey(checkeredcolor)))
     elif bgndcolor != 0:
-        pattern.append(('BACKGROUND', (0, 0), (-1, -1), colors.CMYKColor(black=0.01 * bgndcolor)))
+        pattern.append(('BACKGROUND', (0, 0), (-1, -1), grey(bgndcolor)))
 
     # apply table style
+
     style = TableStyle(pattern)
-    style.add('INNERGRID', (0, 0), (-1, -1), gridline, colors.CMYKColor(black=0.01 * gridcolor))
+    style.add('INNERGRID', (0, 0), (-1, -1), gridline, grey(gridcolor))
     if boxline != 0:
-        style.add('BOX', (0, 0), (-1, -1), boxline, colors.CMYKColor(black=0.01 * boxcolor))
+        style.add('BOX', (0, 0), (-1, -1), boxline, grey(boxcolor))
     table.setStyle(style)
 
     # draw individual graphs
-    for loc_x, loc_y in locations:
+
+    for (loc_x, loc_y) in locations:
         frame = Frame(
-            loc_x + xMargins, loc_y + yMargins, grid_w, grid_h, showBoundary=0,
-            leftPadding=0, bottomPadding=0, rightPadding=0, topPadding=0)
+            loc_x + xMargins,
+            loc_y + yMargins,
+            grid_w,
+            grid_h,
+            leftPadding=0,
+            bottomPadding=0,
+            rightPadding=0,
+            topPadding=0,
+            )
         frame.addFromList([table], page)
 
+
 def dotted(
-        filename, pagesize=letter, margins=0.5*inch, spacer=0.25*inch, gridspace=0.25*inch,
-        dotsize=0.5*mm, boxline=0, dotcolor=50, boxcolor=80, bgndcolor=0,
-        layout=(1, 1), **excessParams):
+    filename,
+    pagesize=letter,
+    margins=0.5 * inch,
+    spacer=0.25 * inch,
+    gridspace=0.25 * inch,
+    dotsize=0.5 * mm,
+    boxline=0,
+    dotcolor=50,
+    boxcolor=80,
+    bgndcolor=0,
+    layout=(1, 1),
+    **excessParams
+    ):
     """
     Generates a page of vertex-dotted grid paper.
 
@@ -141,16 +218,19 @@ def dotted(
     bgndcolor -- color of grid background
     layout -- number of graphs per page in (x, y) tuple
     """
+
     # dimensions and layout
-    up_w, up_h = layout if layout[0] > 0 and layout[1] > 0 else (1, 1)
-    page_w, page_h = pagesize
-    area_w, area_h = (page_w - 2 * margins - (up_w - 1) * spacer) / up_w, \
-                     (page_h - 2 * margins - (up_h - 1) * spacer) / up_h
+
+    (up_w, up_h) = (layout if layout > (0, 0) else (1, 1))
+    (page_w, page_h) = pagesize
+    (area_w, area_h) = ((page_w - 2 * margins - (up_w - 1) * spacer) / up_w,
+                        (page_h - 2 * margins - (up_h - 1) * spacer) / up_h)
+
     if area_w < 0 or area_h < 0:
-        raise ValueError("The specified dimensions do no fit on the page.")
-    
+        raise ValueError('Specified dimensions do not fit on page.')
+
     frame_locs = list()
-    
+
     x = margins  # begin after left margin
     for frame_x in xrange(up_w):
         y = margins  # begin above lower margin
@@ -160,21 +240,49 @@ def dotted(
         x += area_w + spacer
 
     # draw result
+
     page = canvas.Canvas(filename, pagesize=pagesize)
     _makeDottedGrids(
-            page=page, locations=frame_locs, size=(area_w, area_h), gridspace=gridspace,
-            dotsize=dotsize, boxline=boxline, dotcolor=dotcolor, boxcolor=boxcolor, bgndcolor=bgndcolor)
+        page=page,
+        locations=frame_locs,
+        size=(area_w, area_h),
+        gridspace=gridspace,
+        dotsize=dotsize,
+        boxline=boxline,
+        dotcolor=dotcolor,
+        boxcolor=boxcolor,
+        bgndcolor=bgndcolor,
+        )
     placeLogo(margins, pagesize, canvas, quadrant=4)
 
-    page.setTitle("Dotted Graph Paper by Give Sheet")
-    page.setAuthor("Andre Aboulian via Give Sheet")
-    page.setSubject("Page Template")
-    page.setKeywords(['graph','dotted','dots','givesheet','pdf','grid','template','paper'])
+    page.setTitle('Dotted Graph Paper by Give Sheet')
+    page.setAuthor('Andre Aboulian via Give Sheet')
+    page.setSubject('Page Template')
+    page.setKeywords([
+        'graph',
+        'dotted',
+        'dots',
+        'givesheet',
+        'pdf',
+        'grid',
+        'template',
+        'paper',
+        ])
     page.save()
 
+
 def _makeDottedGrids(
-        page, locations, size, gridspace, dotsize, boxline, dotcolor, boxcolor,
-        bgndcolor, **excessParams):
+    page,
+    locations,
+    size,
+    gridspace,
+    dotsize,
+    boxline,
+    dotcolor,
+    boxcolor,
+    bgndcolor,
+    **excessParams
+    ):
     """
     Places a Cartesian graph in the specified location on the canvas.
 
@@ -189,50 +297,75 @@ def _makeDottedGrids(
     boxcolor -- color of box surrounding graph(s)
     bgndcolor -- color of grid background
     """
-    # dimensions
-    area_w, area_h = size
 
-    # compute grid spacing
-    cells_x, cells_y = int(area_w / gridspace), int(area_h / gridspace)
+    # dimensions and spacing
+
+    (area_w, area_h) = size
+
+    (cells_x, cells_y) = (int(area_w / gridspace), int(area_h / gridspace))
     if cells_x < 1 or cells_y < 1:
-        raise ValueError("The specified dimensions do no fit on the page.")
+        raise ValueError('Specified dimensions do not fit on page.')
+
     grid_w = cells_x * gridspace
     grid_h = cells_y * gridspace
+
     xMargins = (area_w - grid_w) / 2
     yMargins = (area_h - grid_h) / 2
 
-    # dots on outer perimeter
     outerDots = bgndcolor == 0 and boxline == 0
 
     # draw individual graphs
-    for loc_x, loc_y in locations:
+
+    for (loc_x, loc_y) in locations:
+
         # draw surrounding box
-        page.setStrokeColor(colors.CMYKColor(black=0.01 * boxcolor))
-        page.setFillColor(colors.CMYKColor(black=0.01 * bgndcolor))
+
+        page.setStrokeColor(grey(boxcolor))
+        page.setFillColor(grey(bgndcolor))
         page.setLineWidth(boxline)
-        page.roundRect(x=loc_x + xMargins, y=loc_y + yMargins, width=grid_w, height=grid_h,
-                       radius=gridspace/2,stroke=(boxline > 0), fill=(bgndcolor != 0))
+        page.roundRect(
+            x=loc_x + xMargins,
+            y=loc_y + yMargins,
+            width=grid_w,
+            height=grid_h,
+            radius=gridspace / 2,
+            stroke=boxline > 0,
+            fill=bgndcolor != 0,
+            )
 
         # starting position and number of dots
+
         x = loc_x + xMargins
         if not outerDots:
             x += gridspace
-            dots_x, dots_y = cells_x - 1, cells_y - 1
+            (dots_x, dots_y) = (cells_x - 1, cells_y - 1)
         else:
-            dots_x, dots_y = cells_x + 1, cells_y + 1
+            (dots_x, dots_y) = (cells_x + 1, cells_y + 1)
 
         # draw dots
+
         for vert_x in xrange(dots_x):
             y = loc_y + yMargins
             if not outerDots:
                 y += gridspace
             for vert_y in xrange(dots_y):
-                page.setFillColor(colors.CMYKColor(black=0.01 * dotcolor))
-                page.circle(x_cen=x, y_cen=y, r=dotsize/2, stroke=0, fill=1)
+                page.setFillColor(grey(dotcolor))
+                page.circle(x_cen=x, y_cen=y, r=dotsize / 2, stroke=0, fill=1)
                 y += gridspace
             x += gridspace
 
 
 if __name__ == '__main__':
-    cartesian("output.pdf", spacer=0.10*inch,boxline=2,gridspace=0.1*inch, bgndcolor=5, gridcolor=0, gridline=1, boxcolor=50, layout=(3,7))
+    cartesian(
+        'output.pdf',
+        spacer=0.1 * inch,
+        boxline=2,
+        gridspace=0.1 * inch,
+        bgndcolor=5,
+        gridcolor=0,
+        gridline=1,
+        boxcolor=50,
+        layout=(3, 7),
+        )
+
     # dotted("output.pdf",layout=(3,4),boxline=0,bgndcolor=5,gridspace=0.25*inch)
